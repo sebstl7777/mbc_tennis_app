7f0a4655fdbb67e7b68fa74e8a5281efe19333e8
STL7 <sebastien.lowy@yale.edu>
2025-06-28 09:40:24 -0400

    url encoded

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:[@tlaSt.Louis7]@db.jfkdvjuishdjdqmnuqay.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres:%40tlaSt.Louis7@db.jfkdvjuishdjdqmnuqay.supabase.co:5432/postgres"
 
 engine = create_engine(
     DATABASE_URL
